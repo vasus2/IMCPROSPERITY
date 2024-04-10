833701b27fd4fb6b5a37768cb8f466d5c8640547
vasus2 <vasus2@illinois.edu>
2024-04-10 16:24:47 -0500

    corrected db4 wavelet. Works exactly as intended

--- db4waveletnp.py
+++ db4waveletnp.py
@@ -1,79 +1,135 @@
-import numpy as np
+class DB4WaveletTransform:
+    """
+    Class to run the selected wavelet and to perform the dwt & idwt
+    based on the wavelet filters
 
-class Daubechies4WaveletTransform:
+    Attributes
+    ----------
+    __wavelet__: object
+        object of the selected wavelet class
     """
-    Class to perform the Discrete Wavelet Transform (DWT) and Inverse DWT (IDWT)
-    using the Daubechies 4 wavelet.
-    """
+
+
 
     def __init__(self):
-        # Daubechies 4 wavelet filter coefficients
-        self.decomp_low = np.array([-0.010597401784997278, 0.032883011666982945, 0.030841381835986965,
-                                   -0.18703481171888114, -0.02798376941698385, 0.6308807679295904,
-                                   0.7148465705525415, 0.23037781330885523])
-        self.decomp_high = np.array([-0.23037781330885523, 0.7148465705525415, -0.6308807679295904,
-                                    -0.02798376941698385, 0.18703481171888114, 0.030841381835986965,
-                                    -0.032883011666982945, -0.010597401784997278])
-        self.recon_low = np.array([0.23037781330885523, 0.7148465705525415, 0.6308807679295904,
-                                  -0.02798376941698385, -0.18703481171888114, 0.030841381835986965,
-                                  0.032883011666982945, -0.010597401784997278])
-        self.recon_high = np.array([-0.010597401784997278, -0.032883011666982945, 0.030841381835986965,
-                                   0.18703481171888114, -0.02798376941698385, -0.6308807679295904,
-                                   0.7148465705525415, -0.23037781330885523])
+        self.__name__ = "Daubechies Wavelet 4"
+        self.__motherWaveletLength__ = 8  # length of the mother wavelet
+        self.__transformWaveletLength__ = 2  # minimum wavelength of input signal
 
-    def dwt(self, arr_time, level):
+        # decomposition filter
+        # low-pass
+        self.decompositionLowFilter = [
+            -0.010597401784997278,
+            0.032883011666982945,
+            0.030841381835986965,
+            - 0.18703481171888114,
+            - 0.02798376941698385,
+            0.6308807679295904,
+            0.7148465705525415,
+            0.23037781330885523
+        ]
+
+        # high-pass
+        self.decompositionHighFilter = [
+            -0.23037781330885523,
+            0.7148465705525415,
+            - 0.6308807679295904,
+            - 0.02798376941698385,
+            0.18703481171888114,
+            0.030841381835986965,
+            - 0.032883011666982945,
+            - 0.010597401784997278,
+        ]
+
+        # reconstruction filters
+        # low pass
+        self.reconstructionLowFilter = [
+            0.23037781330885523,
+            0.7148465705525415,
+            0.6308807679295904,
+            - 0.02798376941698385,
+            - 0.18703481171888114,
+            0.030841381835986965,
+            0.032883011666982945,
+            - 0.010597401784997278,
+        ]
+
+        # high-pass
+        self.reconstructionHighFilter = [
+            -0.010597401784997278,
+            - 0.032883011666982945,
+            0.030841381835986965,
+            0.18703481171888114,
+            - 0.02798376941698385,
+            - 0.6308807679295904,
+            0.7148465705525415,
+            - 0.23037781330885523,
+        ]
+
+    def dwt(self, arrTime, level):
         """
         Discrete Wavelet Transform
 
         Parameters
         ----------
-        arr_time: numpy.ndarray
-            Input array in the Time domain.
-        level: int
-            Level to decompose.
+        arrTime : array_like
+            input array in Time domain
+        level : int
+            level to decompose
 
         Returns
         -------
-        numpy.ndarray
-            Output array in the Frequency or Hilbert domain.
+        array_like
+            output array in Frequency or the Hilbert domain
         """
-        n = len(arr_time)
-        a = level // 2
+        arrHilbert = [0.] * level
+        # shrinking value 8 -> 4 -> 2
+        a = level >> 1
 
-        # Compute approximation and detail coefficients
-        approx = np.convolve(arr_time, self.decomp_low, 'valid')
-        detail = np.convolve(arr_time, self.decomp_high, 'valid')
+        for i in range(a):
+            for j in range(self.__motherWaveletLength__):
+                k = (i << 1) + j
 
-        # Downsample the coefficients
-        arr_hilbert = np.concatenate((approx[::2], detail[::2]))
+                # circulate the array if scale is higher
+                while k >= level:
+                    k -= level
 
-        return arr_hilbert
+                # approx & detail coefficient
+                arrHilbert[i] += arrTime[k] * self.decompositionLowFilter[j]
+                arrHilbert[i + a] += arrTime[k] * self.decompositionHighFilter[j]
 
-    def idwt(self, arr_hilbert, level):
+        return arrHilbert
+
+    def idwt(self, arrHilbert, level):
         """
         Inverse Discrete Wavelet Transform
 
         Parameters
         ----------
-        arr_hilbert: numpy.ndarray
-            Input array in the Frequency or Hilbert domain.
-        level: int
-            Level to decompose.
+        arrHilbert : array_like
+            input array in Frequency or the Hilbert domain
+        level : int
+            level to decompose
 
         Returns
         -------
-        numpy.ndarray
-            Output array in the Time domain.
+        array_like
+            output array in Time domain
         """
-        n = len(arr_hilbert)
-        a = level // 2
+        arrTime = [0.] * level
+        # shrinking value 8 -> 4 -> 2
+        a = level >> 1
 
-        # Upsample the coefficients
-        approx = np.repeat(arr_hilbert[:a], 2)
-        detail = np.repeat(arr_hilbert[a:], 2)
+        for i in range(a):
+            for j in range(self.__motherWaveletLength__):
+                k = (i << 1) + j
 
-        # Reconstruct the time-domain signal
-        arr_time = np.convolve(approx, self.recon_low, 'full') + \
-                   np.convolve(detail, self.recon_high, 'full')
+                # circulating the array if scale is higher
+                while k >= level:
+                    k -= level
 
-        return arr_time[:level]
+                # summing the approx & detail coefficient
+                arrTime[k] += (arrHilbert[i] * self.reconstructionLowFilter[j] +
+                               arrHilbert[i + a] * self.reconstructionHighFilter[j])
+
+        return arrTime
